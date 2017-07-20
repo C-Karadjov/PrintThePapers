15b9e1d987442cb76946351f6a96bee63bcc18ad
molivpopov <mitkop@mail.bg>
2017-07-20 18:36:24 +0300

    small whitespace fixes

--- server/config/app.config.js
+++ server/config/app.config.js
@@ -1,27 +1,25 @@
 /* globals __dirname */
 
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
 const morgan = require('morgan');
-
 const configApp = (app) => {
     app.set('view engine', 'pug');
 
     app.use(bodyParser.json());
 
     app.use(bodyParser.urlencoded({ extended: true }));
-    
+
     app.use(flash());
-    
+
     app.use('/libs',
         express.static(
             path.join(__dirname, '../../node_modules')
         ));
     app.use('/public', express.static('public'));
-    
+
     // app.use(morgan('combined'));
 };
 
--- server/routers/auth.router.js
+++ server/routers/auth.router.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const attachTo = (app, data) => {
     const router = new Router();
     const controllers = require('../controllers/auth.controller')(data);
-    
+
     router
         .get('/login', controllers.getLoginPage)
         .post('/login', controllers.login)
