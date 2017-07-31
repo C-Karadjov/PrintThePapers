79e83ced050e7926ab5067e096fe449ab874c824
molivpopov <mitkop@mail.bg>
2017-07-31 06:03:59 +0300

    minor fixes

--- Design_HTML/js/menu.js
+++ Design_HTML/js/menu.js
@@ -1,18 +1,22 @@
-$(function () {
-   $('#main-nav a').click(function() {
-     $(".mega-menu:not([data-menu='" + $(this).data('menu') + "'])").each(function() {
+/* globals $ */
+$(() => {
+  $('#main-nav a').click(() => {
+    $('.mega-menu:not([data-menu=\'' + $(this).data('menu') + '\'])')
+      .each(() => {
         if ($(this).is(':visible')) {
           $(this).slideToggle(500);
-        }  
-     });
-     $(".mega-menu[data-menu='" + $(this).data('menu') + "']").slideToggle(500);
-   });
-  
-  $('.close-menu').click(function() {
-    $(".mega-menu:visible").slideToggle(500);
+        }
+      });
+    $('.mega-menu[data-menu=\'' + $(this)
+      .data('menu') + '\']')
+      .slideToggle(500);
   });
-  
-  $('#responsive-menu-button').click(function() {
-     $('#responsive-menu').slideToggle(500);
+
+  $('.close-menu').click(() => {
+    $('.mega-menu:visible').slideToggle(500);
+  });
+
+  $('#responsive-menu-button').click(() => {
+    $('#responsive-menu').slideToggle(500);
   });
 });
