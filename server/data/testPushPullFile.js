11efc7a2ee976d00f1ac827f372d1dc28b120941
molivpopov <mitkop@mail.bg>
2017-07-31 06:07:55 +0300

    test

--- server/data/testPushPullFile.js
+++ server/data/testPushPullFile.js
@@ -4,12 +4,6 @@ const id = ObjectID.createFromHexString('59772f761bf40c14140d4d55');
 
 
 // transferFile.push('zaproverka.zip');
-// function stateChange() {
-//     setTimeout(() => {
-//         console.log(transferFile.retId);
-//     }, 2000);
-// }
-// stateChange();
 
 // transferFile.pull('simpleText.txt', '../');
 transferFile.pull('zaproverka.zip', '../');
