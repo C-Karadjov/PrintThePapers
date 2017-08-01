4f6f6a1cd03bafd905fa112d64c2a018f9f8ac95
C-Karadjov <c.karadjov@abv.bg>
2017-08-01 08:20:38 +0300

    small fix

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,7 @@
-/* globals process */
-
 const connectionString = 'mongodb://localhost/PrintThePapers' ||
     'mongodb://172.31.46.210:27017/PrintThePapers';
 
 module.exports = {
-    // eslint-disable-next-line no-process-env
-    port: 80 || 3003,
+    port: 3003,
     connectionString: connectionString,
 };
